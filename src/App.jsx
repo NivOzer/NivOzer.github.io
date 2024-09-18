0191769f9efd0704b57ad005159711a39ae5eede
Niv Ozer <niv.ozer@yahoo.com>
2024-09-19 02:58:58 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 // // Import your images
-// import pillars from './assets/pillars.svg';
-// import pillarsEnd from './assets/pillarsEnd.svg';
-// import godImage from './assets/godImage.webp';
-import pillars from '...';
-import pillarsEnd from '...';
-import godImage from '...';
+import pillars from './assets/pillars.svg';
+import pillarsEnd from './assets/pillarsEnd.svg';
+import godImage from './assets/godImage.webp';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +16,7 @@ function App() {
         <img className="w-1/6" src="/assets/pillarsEnd.svg" alt='RightPillar' />
       </div>
       <div className='content relative z-10 flex justify-center items-center min-h-screen'>
+        <img className="w-1/5" src="/assets/godImage.webp" alt='god' />
         <img className="w-1/5" src="/assets/godImage.webp" alt='god' />
       </div>
     </div>
