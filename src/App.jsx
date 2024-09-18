2dce788f7a247eb225d1c7bd99c5eaf551dbc87f
Niv Ozer <niv.ozer@yahoo.com>
2024-09-19 02:56:36 +0300

    public test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import './index.css'
-// Import your images
-import pillars from './assets/pillars.svg';
-import pillarsEnd from './assets/pillarsEnd.svg';
-import godImage from './assets/godImage.webp';
+// // Import your images
+// import pillars from './assets/pillars.svg';
+// import pillarsEnd from './assets/pillarsEnd.svg';
+// import godImage from './assets/godImage.webp';
+import pillars from '...';
+import pillarsEnd from '...';
+import godImage from '...';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,11 +15,11 @@ function App() {
   return (
     <div className='container relative min-h-screen'>
       <div className='pillars absolute inset-0 flex justify-between z-0'>
-      <img className="w-1/6" src={pillars} alt='LeftPillar'/>
-        <img className="w-1/6" src={pillarsEnd} alt='RightPillar'/>
+      <img className="w-1/6" src="/assets/pillars.svg" alt='LeftPillar' />
+        <img className="w-1/6" src="/assets/pillarsEnd.svg" alt='RightPillar' />
       </div>
       <div className='content relative z-10 flex justify-center items-center min-h-screen'>
-        <img className="w-1/5" src={godImage} alt='god'/>
+        <img className="w-1/5" src="/assets/godImage.webp" alt='god' />
       </div>
     </div>
   )
