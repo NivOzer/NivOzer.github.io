7dcd21d6a6731c5ada0373a32a20b13b5fd7b1c6
Niv Ozer <niv.ozer@yahoo.com>
2024-09-19 17:30:53 +0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,28 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='container relative min-h-screen'>
-      <div className='pillars absolute inset-0 flex justify-between z-0'>
-      <img className="w-1/6" src="/assets/pillars.svg" alt='LeftPillar' />
-        <img className="w-1/6" src="/assets/pillarsEnd.svg" alt='RightPillar' />
+    <div className='flex flex-col pl-24'>
+      <div className='pillars z-0'>
+        <img className="absolute left-0 " src="/assets/pillars.svg" alt='LeftPillar' />
+        <img className="absolute right-0" src="/assets/pillarsEnd.svg" alt='RightPillar' />
       </div>
-      <div className='content relative z-10 flex justify-center items-center min-h-screen'>
-        <img className="w-1/5" src="/assets/godImage.webp" alt='god' />
-        <h1>Niv</h1>
+
+
+      <nav className='w-full z-10 flex items-center p-4 text-brown-800 gap-8'>
+        <div className='logo'>
+          <img src="https://cdn.worldvectorlogo.com/logos/roma-3.svg" alt='Rome Logo' className='h-10' />
+        </div>
+        <ul className='nav-links flex space-x-4'>
+          <li><a href='#home' className='hover:text-yellow-500'>Home</a></li>
+          <li><a href='#about' className='hover:text-yellow-500'>About</a></li>
+          <li><a href='#contact' className='hover:text-yellow-500'>Contact</a></li>
+        </ul>
+      </nav>
+
+      <div className='content w-full z-1 flex items-center'>
+        <img className="w-64 h-64" src="/assets/godImage.webp" alt='god' />
       </div>
+
     </div>
   )
 }
