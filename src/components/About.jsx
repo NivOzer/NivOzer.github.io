9e88b9c580125eaa205ebf2686786514590d2cf9
Niv Ozer <niv.ozer@yahoo.com>
2024-09-23 00:14:47 +0300

    Medium screen size first update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,9 @@ export const About = () => {
     <div id="about" className='min-h-screen'>
       <div class="flex flex-col items-center justify-center min-h-screen">
 
-        <div class="xs:max-h-[85vh] xs:justify-around  xs:w-full leading-6 border-2 border-dashed shadow-stitched border-romanBeige  bg-white flex flex-col xs:gap-4 gap-6 items-center rounded-lg p-12 w-1/5 text-center">
-          
+        <div class="xs:p-2 xs:max-h-screen  justify-start xs:w-full leading-6 border-2 border-dashed shadow-stitched border-romanBeige  bg-white flex flex-col xs:gap-4 gap-6 items-center rounded-lg lg:p-12  w-1/5 text-center">
           <div>
-            <img class="xs:w-36 xs:h-36 w-40 h-40 object-cover rounded-full" src="/assets/Me.jpg" alt="god" />
+            <img className="xs:w-28 xs:h-28 w-40 h-40 object-cover rounded-full" src="/assets/Me.jpg" alt="god" />
           </div>
           
           <div className='flex flex-col gap-2'>
@@ -16,11 +15,11 @@ export const About = () => {
             <h2 class="xs:text-sm text-xl font-semibold text-gray-500">Fullstack & Mobile Developer</h2>
           </div>
           
-          <div>
+          <div className=''>
             <p class="xs:text-xs xs:mb-4 text-lg text-gray-700 mb-6 flex-wrap flex">
-            With 3 years of web development experience, I build software like the Romans built their empire—structured, scalable, and built to last. From React to Node, I design applications with the same attention to architecture as the Colosseum.
+            With 3 years of web development experience, I build software like the Romans built their empire - structured, scalable, and built to last. From React to Node, I design applications with the same attention to architecture as the Colosseum.
             Whether it’s coding or debugging, I approach every project with strategy and precision.
-            Hire me, and your projects will stand the test of time—because just like Rome, great apps need solid architecture!</p>
+            Hire me, and your projects will stand the test of time - because just like Rome, great apps need solid architecture!</p>
             <div class="bg-romanBeige h-0.5 opacity-75"></div>
           </div>
 
--- src/components/IntroBlock.jsx
+++ src/components/IntroBlock.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const IntroBlock = () => {
   return (
-    <div id="home" className='flex flex-col gap-8 pt-8 min-h-screen items-center justify-center'>
-      <img className="xs:w-48 xs:h-48 w-64 h-64 mb-4 rounded-full" src="/assets/godImage.webp" alt='god' />
+    <div id="home" className=' flex flex-col xs:gap-4 gap-8 pt-8 min-h-screen items-center justify-center'>
+      <img className="xs:w-32 xs:h-32 w-64 h-64 xs:m-0 mb-4 rounded-full" src="/assets/godImage.webp" alt='god' />
         <div className='flex flex-row gap-12'>
             <div className="flex flex-col gap-4 item justify-center font-cinzel">
                 <h1 className="text-4xl font-bold">Niv Ozer</h1>
@@ -16,6 +16,7 @@ export const IntroBlock = () => {
               <div className='bg-romanBeige h-0.5 opacity-75 mt-4'></div>
             </div>
         </div>
+        {/* <div className='pb-128'></div> */}
     </div>
   )
 }
